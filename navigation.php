44bb21f38ffdb7c07796d9cba238031a24f2db41
stds01 <161261652+stds01@users.noreply.github.com>
2024-07-23 19:14:40 +0200

    Delete navigation.php

--- navigation.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <a href="index.php">Startseite</a>
-    <a href="grundidee.php">Grundidee</a>
-    <a href="dieredaktion.php">Die Redaktion</a>
-    <a href="Archiv.php">Archiv</a>
-    <a href="Abonnement.php">Abonnement</a> 
-
-</nav>
